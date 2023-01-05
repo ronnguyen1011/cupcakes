41b15046fc2a6e3b68787a7787d82c60ce7bb494
Ron Nguyen <ronnguyen1011@gmail.com>
2023-01-05 17:55:24 -0500

    Create home page

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!--
+Names: Ron Nguyen, Jack (Zhenhuai) Zeng
+Date: 1/5/2023
+URL: https://github.com/ronnguyen1011/cupcakes ||| https://nguyenron.greenriverdev.com/328/cupcakes/
+Description: This program contains a form for a cupcake fundraiser. The program will show the options and total price
+that the user selected
+-->
+
 <!doctype html>
 <html lang="en">
 <head>
