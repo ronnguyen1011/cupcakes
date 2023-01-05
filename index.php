af2e6ebb2f117b912bcfc31c1f43fb0d9223bda9
Ron Nguyen <ronnguyen1011@gmail.com>
2023-01-05 17:19:07 -0500

    Create home page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>cupcake</title>
+</head>
+<body>
+<
+<h1>Cupcake Fundraiser</h1>
+<form method = "post" action = "confirm.php">
+    <p>Your Name:</p>
+    <input type="text" value = "Please input your name">
+    <p>Cupcake flavors</p>
+    <input type = 'checkbox' name = 'flavor[]'> The Grasshopper  <br>
+    <input type = 'checkbox' name = 'flavor[]'> Whiskey Maple Bacon <br>
+    <input type = 'checkbox' name = 'flavor[]'> Carrot Walnut <br>
+    <input type = 'checkbox' name = 'flavor[]'> Salted Caramel Cupcake <br>
+    <input type = 'checkbox' name = 'flavor[]'> Red Velvet <br>
+    <input type = 'checkbox' name = 'flavor[]'> Lemon Drop <br>
+    <input type = 'checkbox' name = 'flavor[]'> Tiramisu <br> <br>
+    <input type = 'submit' value = "Order">
+</form>
+
+
+</body>
+</html>
